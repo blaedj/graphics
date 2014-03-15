22af99ff0cc0c14d34148af80ee254efd9070fbf
Blaed <blaedj@gmail.com>
2014-03-15 12:26:59 -0500

    update run script

--- runmain.sh
+++ runmain.sh
@@ -1,2 +1,7 @@
-cd raytracer/
-./write_images -w 100 -h 100 -i ../../src/petesCode/SceneFiles_TestA/oneSphere.xml -o testoutput.png -v
+cd ~/code/cpp/graphics/build/raytracer/
+rm core
+#./write_images -w 750 -h 750 -i ../../src/petesCode/SceneFiles_TestA/simpleTwoSpheres.xml -o simple2spheres.png -v
+#./write_images -w 750 -h 750 -i ../../src/petesCode/SceneFiles_TestA/threeTriangles.xml -o testoutputTriangles.png -v
+#./write_images -w 750 -h 750 -i ../../src/petesCode/SceneFiles_TestA/spheres_1K.xml -o manySpheres.png -v
+#./write_images -w 750 -h 750 -i ../../src/petesCode/SceneFiles_TestA/threeSpheres.xml -o testoutput.png -v
+./write_images -w 750 -h 750 -i ../../src/petesCode/SceneFiles_TestA/oneSphere.xml -o testoutput.png -v
