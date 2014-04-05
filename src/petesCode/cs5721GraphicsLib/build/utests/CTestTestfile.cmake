35165ec377f899f892bfc7a3c37caa8d9b9fd7b7
Blaed <blaedj@gmail.com>
2014-04-05 15:24:48 -0500

    delete build files

--- src/petesCode/cs5721GraphicsLib/build/utests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blaed/code/graphics/src/petesCode/cs5721GraphicsLib/utests
-# Build directory: /home/blaed/code/graphics/src/petesCode/cs5721GraphicsLib/build/utests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(utest_Success "/home/blaed/code/graphics/src/petesCode/cs5721GraphicsLib/build/utests/utest_Success")
-ADD_TEST(utest_Failure "/home/blaed/code/graphics/src/petesCode/cs5721GraphicsLib/build/utests/utest_Failure")
-ADD_TEST(utest_Vector3D "/home/blaed/code/graphics/src/petesCode/cs5721GraphicsLib/build/utests/utest_Vector3D")
